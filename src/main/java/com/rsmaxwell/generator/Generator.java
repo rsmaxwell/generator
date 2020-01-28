7d1c5f4d6c8e6263995d16fd25ce0070e4162a8c
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-01-28 19:11:08 +0000

    update images dir

--- src/main/java/com/rsmaxwell/generator/Generator.java
+++ src/main/java/com/rsmaxwell/generator/Generator.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -86,36 +88,45 @@ public class Generator {
 			int previousDay = 0;
 
 			for (OutputDay day : setOfDays) {
+				try {
+					if (previousYear != day.year) {
+						String key = String.format("%04d", day.year);
+						sb.append("<h2>" + key + "</h2>");
+					}
 
-				if (previousYear != day.year) {
-					String key = String.format("%04d", day.year);
-					sb.append("<h2>" + key + "</h2>");
+					if (previousMonth != day.month) {
+						String key = Month.toString(day.month);
+						sb.append("<h3>" + key + "</h3>");
+					}
+
+					if (previousDay != day.day) {
+						LocalDate localDate = LocalDate.of(day.year, day.month, day.day);
+						DayOfWeek dayOfWeek = DayOfWeek.from(localDate);
+						int val = dayOfWeek.getValue();
+
+						String key = String.format("%s  %02d     %s", Day.toString(val), day.day, day.reference);
+						sb.append("<h4>" + key + "</h4>");
+					}
+
+					if ((previousYear != day.year) && (previousMonth == day.month) && (previousDay == day.day)) {
+						sb.append(" ");
+					}
+					sb.append(day.html.trim());
+
+					previousYear = day.year;
+					previousMonth = day.month;
+					previousDay = day.day;
+				} catch (Exception e) {
+					String key = String.format("%04d-%02d-%02d  %s    %s", day.year, day.month, day.day, day.order,
+							day.reference);
+					throw new Exception("day: " + key, e);
 				}
-
-				if (previousMonth != day.month) {
-					String key = Month.toString(day.month);
-					sb.append("<h3>" + key + "</h3>");
-				}
-
-				if (previousDay != day.day) {
-					String key = String.format("%s  %02d     %s", Day.toString(day.day), day.day, day.reference);
-					sb.append("<h4>" + key + "</h4>");
-				}
-
-				if ((previousYear != day.year) && (previousMonth == day.month) && (previousDay == day.day)) {
-					sb.append(" ");
-				}
-				sb.append(day.html.trim());
-
-				previousYear = day.year;
-				previousMonth = day.month;
-				previousDay = day.day;
 			}
 
 			String regex = "[\\s]*[\\.]{3}</p> <p>[\\.]{3}[\\s]*";
 			String html = sb.toString().replaceAll(regex, " ");
 
-			File htmlDir = new File(outputDir, "html");
+			File htmlDir = new File(outputDir, "html/" + Integer.toString(year));
 			htmlDir.mkdirs();
 
 			File htmlFile = new File(htmlDir, Integer.toString(year) + ".html");
@@ -124,13 +135,21 @@ public class Generator {
 				writer.write(html);
 			}
 
-			File pdfDir = new File(outputDir, "pdf");
-			pdfDir.mkdirs();
+			File absolutePdfDir = null;
+			if (outputDir.isAbsolute()) {
+				absolutePdfDir = new File(outputDir, "pdf");
+			} else {
+				String currentDirName = System.getProperty("user.home");
+				File absoluteOutputDir = new File(currentDirName, outputDir.getName());
+				absolutePdfDir = new File(absoluteOutputDir.getName(), "pdf");
+			}
+			absolutePdfDir.mkdirs();
 
-			File pdfFile = new File(pdfDir, Integer.toString(year) + ".pdf");
+			File pdfFile = new File(absolutePdfDir, Integer.toString(year) + ".pdf");
+			String baseUri = "output/html/" + Integer.toString(year);
 
 			ConverterProperties properties = new ConverterProperties();
-			properties.setBaseUri("output/html");
+			properties.setBaseUri(baseUri);
 			HtmlConverter.convertToPdf(htmlFile, pdfFile, properties);
 		}
 	}
